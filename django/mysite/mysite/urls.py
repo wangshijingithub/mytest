59d5b24980f8d4569b44bf294a91e380335cd95c
unknown <632172230@qq.com>
2016-11-15 16:37:25 +0800

    templates

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -2,11 +2,10 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
-from mysite.views import hello, current_time, hours_ahead
+from mysite.views import current_time, hours_ahead
 
 urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'^hello/$', hello),
 	url(r'^time/$', current_time),
 	url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 ]
--- django/mysite/mysite/views.py
+++ django/mysite/mysite/views.py
@@ -1,15 +1,17 @@
 ﻿# -*- coding:utf-8 -*-
+'''from django.template.loader import get_template
+from django.template import Context
+from django.http import Http404, HttpResponse'''
+from django.http import Http404
+from django.shortcuts import render_to_response
 import datetime
-from django.http import Http404, HttpResponse
-
-
-def hello(request):
-	return HttpResponse('Hello world')
 
 def current_time(request):
 	now = datetime.datetime.now()
-	html = "<html><body>It is now %s.</body></html>" %now
-	return HttpResponse(html)
+	'''t = get_template('current_datetime.html')
+	html = t.render(Context({'current_date':now}))
+	return HttpResponse(html)'''
+	return render_to_response("current_datetime.html", {"current_date":now})
 
 def hours_ahead(request, offset):
 	try:
@@ -18,5 +20,6 @@ def hours_ahead(request, offset):
 		raise Http404()
 
 	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	html = "<html><body>In %s hour(s), it will be %s.</body></html>" %(offset, dt)
-	return HttpResponse(html)
+	'''html = "<html><body>In %s hour(s), it will be %s.</body></html>" %(offset, dt)
+	return HttpResponse(html)'''
+	return render_to_response("hours_head.html", {"hour_offset":offset, "next_time":dt})
